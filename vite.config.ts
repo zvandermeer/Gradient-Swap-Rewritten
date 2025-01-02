1d044a8e42d48622c7f951805067ebb6ad833e1a
Zoey VanderMeer <zoeyvanderm@gmail.com>
2025-01-01 22:43:38 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -26,25 +26,25 @@ export default defineConfig({
                 display: "standalone",
                 icons: [
                     {
-                        src: "/icons/pwa-192x192.png",
+                        src: "/src/assets/icons/pwa-192x192.png",
                         sizes: "192x192",
                         type: "image/png",
                         purpose: "any",
                     },
                     {
-                        src: "/icons/pwa-512x512.png",
+                        src: "/src/assets/icons/pwa-512x512.png",
                         sizes: "512x512",
                         type: "image/png",
                         purpose: "any",
                     },
                     {
-                        src: "/icons/pwa-maskable-192x192.png",
+                        src: "/src/assets/icons/pwa-maskable-192x192.png",
                         sizes: "192x192",
                         type: "image/png",
                         purpose: "maskable",
                     },
                     {
-                        src: "/icons/pwa-maskable-512x512.png",
+                        src: "/src/assets/icons/pwa-maskable-512x512.png",
                         sizes: "512x512",
                         type: "image/png",
                         purpose: "maskable",
